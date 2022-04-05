2d580f6b606de875e7d2aa75ff42a4a527082d5e
rayoung <rlafkdud122i@gmail.com>
2022-04-05 22:34:51 +0900

    modal and transition

--- vue-intermediate/vue-todo/src/components/TodoInput.vue
+++ vue-intermediate/vue-todo/src/components/TodoInput.vue
@@ -1,49 +1,78 @@
 <template>
-    <div class="inputBox shadow"> 
+    <div class="inputBox shadow">
         <!-- v-model은  input에 입력된 값을 동적으로 바로바로  vue인스턴스 안에 매핑하는 역할-->
         <!-- v-model을 사용하기 위해서는 특정데이터와 연결시켜줘야 함
         즉, input에 입력된 값이 vue 인스턴스의 특정 데이터와 매핑 -->
         <!-- 여기서는 newTodoItem이라는 뷰인스턴스의 데이터와 매핑 -->
-        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
         <!-- v-on:click="클릭했을 때 동작할 메소드 이름" -->
         <span class="addContainer">
             <i class="fa-solid fa-plus addBtn" v-on:click="addTodo"></i>
         </span>
+        <Modal v-if="showModal" @close="showModal = false">
+            <!--
+        you can use custom content here to overwrite
+        default content : 너가 원하는 content를 이용해서 이 모달을 다시 재정의할 수 있다는 것을 의미
+    -->
+            <!-- modal에 있는 header부분을 가져와서 이렇게 수정된 것을 의미함
+                <div class="modal-header">
+                    <h3 slot=header">
+                        경고
+                    </h3>
+                <div>
+            -->
+            <h3 slot="header">
+                <!-- custom header -->
+                경고!
+                <i
+                    class="closeModalBtn fa-solid fa-xmark"
+                    @click="showModal = false"
+                ></i>
+            </h3>
+            <h3 slot="body">무언가를 입력하세요.</h3>
+            <h3 slot></h3>
+        </Modal>
     </div>
 </template>
 
 <script>
+//Modal 컴포넌트에 대한 기능을 Modal이라는 객체 에 넣어줌
+import AlertModal from "./common/AlertModal.vue";
 export default {
-    data : function() {
+    data: function () {
         return {
             // 새로운 목록을 생성할 때 todoInput만의 새로운 데이터인 newTodoItem를 바로 todoItem에 추가해주는 것이 좋음
-            newTodoItem: ""
-
-        }
+            newTodoItem: "",
+            showModal: false,
+        };
     },
     methods: {
-        addTodo: function(){
+        addTodo: function () {
             //여기서 this는 todoInput을 가르킴
             //TodoHeader, TodoInput, TodoList, TodoFooter은 App.vue에 등록이 되어있고 이 App.vue는 main.js에 등록이 되어있음 결과적으로 App하나에 등록
             // console.log(this.newTodoItem);
             //여기서 문제점 --> 바로바로 아래 todolist에 반영이 되지 않고 새로고침을 눌러야 반영이 됨 --> 이유 : todoLIst와 todoInput이 연결되어있지 않기 때문
             //여기에 저장하는 로직
-            if (this.newTodoItem !== '') {
+            if (this.newTodoItem !== "") {
                 //지금 바로 위 컴포넌트인 App.vue로 newTodoItem을 보내줘야 함
                 //이유 : todoList를 바로 업데이트 하기 위해서는 App.vue로 입력된 목록을 보내서 다시 TodoList로 내려줘야 하기 때문
                 // this.$emit('이벤트 이름', 인자1, 인자2, 인자3,,,,)
                 //$emit을 하게되면 todoInput이라는 컴포넌트에서 addTodoItem이라는 이벤트를 발생시킴, 그리고 그게  App.vue로 올라와서 매핑시키면 메서드가 실행됨
-                this.$emit('addTodoItem',this.newTodoItem);
+                this.$emit("addTodoItem", this.newTodoItem);
                 this.clearInput();
+            } else {
+                this.showModal = true;
             }
-            
         },
         clearInput: function () {
-            this.newTodoItem = '';
-        }
-
-    }
-}
+            this.newTodoItem = "";
+        },
+    },
+    components: {
+        //위의 Modal 객체를 Modal태그에 연결
+        Modal: AlertModal,
+    },
+};
 </script>
 
 <style scoped>
@@ -62,7 +91,7 @@ input:focus {
 }
 .addContainer {
     float: right;
-    background: linear-gradient(to right, #6478FB, #8763FB);
+    background: linear-gradient(to right, #6478fb, #8763fb);
     display: block;
     width: 3rem;
     border-radius: 0 5px 5px 0;
@@ -70,5 +99,8 @@ input:focus {
 .addBtn {
     color: white;
     vertical-align: middle;
+}
+.closeModalBtn {
+    color: #42b983;
 }
 </style>
--- vue-intermediate/vue-todo/src/components/TodoList.vue
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -1,43 +1,57 @@
 <template>
     <div>
-        <ul>
+        <transition-group name="list" tag="ul">
             <!-- v-bind:key는 중복되지 않는 텍스트가 키가 되어서  중복되지 않는 선에서 키가 유일해서 vue-for의 성능을 가속화-->
             <!-- 여기서 index는 v-for의 고유의 문법으로 몇 번째인지 인덱스 번호를 부여해준다 -->
             <!-- 즉, v-for안에서 todoItem과 index로 데이터를 접근할 수 있다 -->
-            <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+            <li
+                v-for="(todoItem, index) in propsdata"
+                v-bind:key="todoItem.item"
+                class="shadow"
+            >
                 <!-- 하나의 리스트가 만들어질 때마다 아래의 내용이 동일하게 입력되므로 어떤 리스트의 데이터를 눌러도 아래의 내용이 똑같이 작동됨 -->
                 <!-- 위해서 토글을 눌러서 completed상태로 하게 된다면 v-on:click에 의해서 toggleComplete가 실행됨 -->
                 <!-- 체크박스를 클릭하면 toggleComplete라는 이벤트가 발생, 즉, 토글이 클릭했을때 이벤트를 toggleComplete라고 이름 짓은 것을 의미 -->
-                <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+                <i
+                    class="checkBtn fa-solid fa-check"
+                    v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+                    v-on:click="toggleComplete(todoItem, index)"
+                ></i>
                 <!-- v-bind:class은 기존의 html속성에다가  동적인 값을 부여하는 것을 의미함
                 즉, 여기서는 todoItem.completed에 따라서 속성이 부여됨, 만약에 completed가 false이면 이 class는 없어짐 true라면 textCompleted만 남아서 속성이 적용됨-->
-                <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
-                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                <span v-bind:class="{ textCompleted: todoItem.completed }">{{
+                    todoItem.item
+                }}</span>
+                <span
+                    class="removeBtn"
+                    v-on:click="removeTodo(todoItem, index)"
+                >
                     <i class="fa-solid fa-trash-can"></i>
                 </span>
             </li>
-        </ul>
+        </transition-group>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['propsdata'],
+    props: ["propsdata"],
     methods: {
-        removeTodo: function(todoItem, index) {
+        removeTodo: function (todoItem, index) {
             //즉, 삭제하라는 클릭이벤트를 removeItem이라는 이름으로 받아줌, 그리고 App.vue에 넘겨줌 그리고 App.vue는 이 이벤트를 받으면 매핑된 함수를 실행시켜준다.
-            this.$emit('removeItem', todoItem, index);
+            this.$emit("removeItem", todoItem, index);
             console.log(todoItem);
         },
-        toggleComplete: function(todoItem, index) { //할 일 완료 기능
-            this.$emit('toggleItem', todoItem, index);
-        }
-    }
-}
+        toggleComplete: function (todoItem, index) {
+            //할 일 완료 기능
+            this.$emit("toggleItem", todoItem, index);
+        },
+    },
+};
 </script>
 
 <style scoped>
-    ul {
+ul {
     list-style-type: none;
     padding-left: 0px;
     margin-top: 0;
@@ -55,12 +69,12 @@ li {
 }
 .checkBtn {
     line-height: 45px;
-  /* color: black; */
+    /* color: black; */
     color: #62acde;
     margin-right: 5px;
 }
 .checkBtnCompleted {
-  /* color: #62acde; */
+    /* color: #62acde; */
     color: black;
 }
 .textCompleted {
@@ -69,5 +83,14 @@ li {
 .removeBtn {
     margin-left: auto;
     color: #de4343;
+}
+/* 리스트 아이템 트랜지션 효과 */
+.list-enter-active,
+.list-leave-active {
+    transition: all 1s; /*트랜지션이 1초 정도 유지됨 */
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(30px);
 }
 </style>
